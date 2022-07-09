11a10a1d1eb9d5d279a883261bb09b72d1ef66b6
Rabii Kahlaoui <rabii.kahlaouiii@gmail.com>
2022-07-09 19:26:21 +0100

    Configure Hardhat and deploy Smart contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
-require("@nomicfoundation/hardhat-toolbox");
+// require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+const dotenv = require("dotenv");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+dotenv.config();
+
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.7",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_ETH,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY,
+  }
 };
